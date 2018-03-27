3855ee62077cb82e36406eea5f759a33b8a9c44c
Jan Brecka <jan.brecka@gmail.com>
2018-03-27 15:26:03 +0200

    add bin/rountrip to be able to be tested against official test suite

---
+++ bin/roundtrip.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use transit\JSONReader;
+use transit\JSONWriter;
+use transit\Transit;
+
+$input = file_get_contents($argv[1]);
+$transit = new Transit(new JSONReader(), new JSONWriter());
+$output = $transit->write($transit->read($input));
+echo $output;
